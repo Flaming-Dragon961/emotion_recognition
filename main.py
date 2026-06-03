284108f17b749c18ee3292b225320912050b4591
Flaming-Dragon961 <aab936406@gmail.com>
2026-06-03 19:32:20 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,48 @@
+from deepface import DeepFace
+import cv2
+
+images = {
+    "happy": "img/happy.jpg",
+    "sad": "img/sad.png",
+    "fear": "img/fear.png",
+    "neutral": "img/neutral.png",
+    "surprise": "img/surprise.png",
+    "angry": "img/angry.jpg"
+}
+
+camera = cv2.VideoCapture(0)
+
+count = 0
+emotion = "neutral"
+while True:
+    read, frame = camera.read()
+    if not read:
+        break
+    count+=1
+    try:
+        if count % 10 == 0:
+            results = DeepFace.analyze(frame,actions=["emotion"],enforce_detection=False)
+            if isinstance(results, dict):
+                emotion = results["dominant_emotion"]
+            else:
+                emotion = results[0]["dominant_emotion"]
+        cv2.putText(frame, emotion, (50, 50),cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        cv2.putText(frame, "Press Q to quit", (10, 470),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
+        emotion_img_path = images.get(emotion, "img/neutral.png")
+        emotion_img = cv2.imread(emotion_img_path)
+        frame_resized = cv2.resize(frame, (640, 480))
+        if emotion_img is not None:
+            emotion_img = cv2.resize(emotion_img, (300, 480))
+            side_by_side = cv2.hconcat([frame_resized, emotion_img])
+            cv2.imshow("Face Emotion Recognition", side_by_side)
+        else:
+            cv2.imshow("Face Emotion Recognition", frame_resized)
+
+    except Exception as e:
+        cv2.imshow("Face Emotion Recognition", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+camera.release()
+cv2.destroyAllWindows()
